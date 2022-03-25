02a636c8e7ab972d79a1034d2686954c050c6d3d
66666666 <100743005+xiaozhe12@users.noreply.github.com>
2022-03-25 16:37:28 +0800

    Add files via upload

---
+++ 03.26/xz1.php
@@ -0,0 +1,15 @@
+<?php
+$a = true;
+$b = false;
+$username = "Mike";
+if ($username == "Mike") {
+    echo "helloMike<br>";
+}
+if ($a == true) {
+    echo "a为真<br>";
+}
+if ($b == false) echo "b为真<br>";
+else echo "谢谢";
+echo '输出';
+
+
